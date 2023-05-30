affd3ade2cdbab44b7f7f6718aacf05c25f3d7b6
Deric San Andres <105538170+dericsadrs@users.noreply.github.com>
2023-05-31 00:08:33 +0800

    Progression 5/31/23

--- Day 3/Rollercoaster.py
+++ Day 3/Rollercoaster.py
@@ -11,5 +11,9 @@ if height > 120:
         print("Please pay $7")
     else:
         print("Please pay $12")
+    wants_photo = input("Do you want a photo taken? Y or N.")
+    if wants_photo == "Y":
+       bill+=3
+    print(f"Your final bill is ${bill}")
 else:
     print("Sorry you have to grow taller before you can ride.")
