21ca8bd4d91f83fe69d1f19177260e063bd004f5
Deric San Andres <105538170+dericsadrs@users.noreply.github.com>
2023-06-03 21:13:38 +0800

    Progression 6/3/23

---
+++ Day 8/function_with_inputs.py
@@ -0,0 +1,11 @@
+#
+# Created on Sat Jun 03 2023
+# Created by Software Engineer Deric San Andres
+#
+def greet(): 
+    
+    print("Even when the")
+    print("Night Changes")
+    print("It will never change baby")
+    
+greet()
