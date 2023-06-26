e034aa1145551d4bcb2476220a4de794c797356e
Deric San Andres <105538170+dericsadrs@users.noreply.github.com>
2023-06-26 10:23:26 +0800

    Day 33 Progression 6/26/23

--- Day 33/request_long_lat.py
+++ Day 33/request_long_lat.py
@@ -6,9 +6,15 @@ MY_LONG = -0.127758
 parameters = {
     "lat" : MY_LAT,
     "lng" : MY_LONG,  
+    "formatted" : 0,
 }
 
 response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
 data = response.json()
-print(data["results"])
+data_results = data["results"]
+sunrise = data_results["sunrise"].split("T")[1].split(":")[0]
+sunset = data_results["sunset"].split("T")[1].split(":")[0]
+
+print(sunrise)
+print(sunset)
