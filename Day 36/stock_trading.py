752a8bc497b0fe98330ac6917fed532a5722e476
Deric San Andres <105538170+dericsadrs@users.noreply.github.com>
2023-06-30 00:17:57 +0800

    Progressions 6/30/23

---
+++ Day 36/stock_trading.py
@@ -0,0 +1,45 @@
+
+import requests
+
+
+STOCK = "TSLA"
+COMPANY_NAME = "Tesla Inc"
+
+## STEP 1: Use https://www.alphavantage.co
+# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+
+STOCK_API_KEY =  "5UH6MTVOWPSC3PAR"
+query_stock_api = "https://www.alphavantage.co/query"
+
+
+stock_api_params = {
+    "function" : "TIME_SERIES_DAILY",
+     "symbol" : STOCK,
+     "apikey" : STOCK_API_KEY,
+    
+}
+
+response = requests.get(url=query_stock_api,params=stock_api_params)
+response.raise_for_status()
+
+print(response.json())
+
+
+## STEP 2: Use https://newsapi.org
+# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
+
+## STEP 3: Use https://5UH6MTVOWPSC3PARwww.twilio.com
+# Send a seperate message with the percentage change and each article's title and description to your phone number. 
+
+
+#Optional: Format the SMS message like this: 
+"""
+TSLA: 🔺2%
+Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
+Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
+or
+"TSLA: 🔻5%
+Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
+Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
+"""
+
