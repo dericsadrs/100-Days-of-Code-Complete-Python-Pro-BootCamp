409803bc595ea31287e33da5473ed48a0537ad2f
Deric San Andres <105538170+dericsadrs@users.noreply.github.com>
2023-07-04 23:48:51 +0800

    7/4/2023 Progression

---
+++ Day 39/data_manager.py
@@ -0,0 +1,40 @@
+import requests
+import pandas
+
+SHEETY_PRICES_ENDPOINT = "https://api.sheety.co/a6604531c6e5d07f06602644d6224317/copyOfFlightDeals/prices"
+
+class DataManager:
+
+    def __init__(self):
+        self.destination_data = {}
+
+    def get_destination_data(self):
+        # 2. Use the Sheety API to GET all the data in that sheet and print it out.
+        response = requests.get(url=SHEETY_PRICES_ENDPOINT)
+        data = response.json()
+        self.destination_data = data["prices"]
+        return self.destination_data
+
+    # 6. In the DataManager Class make a PUT request and use the row id from sheet_data
+    # to update the Google Sheet with the IATA codes. (Do this using code).
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            new_data = {
+                "price": {
+                    "iataCode": city["iataCode"]
+                }
+            }
+            response = requests.put(
+                url=f"{SHEETY_PRICES_ENDPOINT}/{city['id']}",
+                json=new_data
+            )
+            print(response.text)
+
+
+
+# data_manager_object = DataManager()
+# get_destination_data = data_manager_object.get_destination_data()
+
+# pd_get_destination_data = pandas.DataFrame(get_destination_data)
+
+# print(pd_get_destination_data)
---
+++ Day 40/main.py
@@ -0,0 +1,7 @@
+print ("Welcome to Deric's Flight Club. \nWe find the best flight deals and email you.")
+
+first_name = input("What is your first name?")
+last_name = input("What is your last name?")
+
+email = input("")
+email_confirmation = input("")
