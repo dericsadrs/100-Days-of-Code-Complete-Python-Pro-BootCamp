759d2daae564c38b09e55dcc67f0ef7a250931de
Deric San Andres <105538170+dericsadrs@users.noreply.github.com>
2023-07-06 11:23:40 +0800

    7/6/2023 Progression

--- Day 40/notification_manager.py
+++ Day 40/notification_manager.py
@@ -1,14 +1,18 @@
+import smtplib
 from twilio.rest import Client
-import os
 
-TWILIO_VIRTUAL_NUMBER = YOUR TWILIO VIRTUAL NUMBER
-TWILIO_VERIFIED_NUMBER = YOUR TWILIO VERIFIED NUMBER
-
+TWILIO_SID =" YOUR TWILIO ACCOUNT SID"
+TWILIO_AUTH_TOKEN = "YOUR TWILIO AUTH TOKEN"
+TWILIO_VIRTUAL_NUMBER = "YOUR TWILIO VIRTUAL NUMBER"
+TWILIO_VERIFIED_NUMBER = "YOUR TWILIO VERIFIED NUMBER"
+MAIL_PROVIDER_SMTP_ADDRESS = "YOUR EMAIL PROVIDER SMTP ADDRESS smtp.gmail.com"
+MY_EMAIL = "YOUR EMAIL"
+MY_PASSWORD = "YOUR PASSWORD"
 
 class NotificationManager:
 
     def __init__(self):
-        self.client = Client(os.getenv("TWILIO_SID"), os.getenv("TWILIO_AUTH_TOKEN"))
+        self.client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
 
     def send_sms(self, message):
         message = self.client.messages.create(
@@ -16,5 +20,15 @@ class NotificationManager:
             from_=TWILIO_VIRTUAL_NUMBER,
             to=TWILIO_VERIFIED_NUMBER,
         )
-        # Prints if successfully sent.
         print(message.sid)
+
+    def send_emails(self, emails, message):
+        with smtplib.SMTP(MAIL_PROVIDER_SMTP_ADDRESS) as connection:
+            connection.starttls()
+            connection.login(MY_EMAIL, MY_PASSWORD)
+            for email in emails:
+                connection.sendmail(
+                    from_addr=MY_EMAIL,
+                    to_addrs=email,
+                    msg=f"Subject:New Low Price Flight!\n\n{message}".encode('utf-8')
+                )
