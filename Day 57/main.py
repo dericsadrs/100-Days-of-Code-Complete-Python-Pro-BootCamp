710f4806b6d0fe99232de550776d916b4df30fed
Deric San Andres <105538170+dericsadrs@users.noreply.github.com>
2023-08-03 22:35:57 +0800

    Progress 8/3/2023

--- Day 57/main.py
+++ Day 57/main.py
@@ -1,11 +1,14 @@
+import random, datetime
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+year_now = datetime.datetime.now().year
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    random_number = random.randint(1,10)
+    return render_template("index.html", num = random_number,year = year_now )
 
 
 if __name__ == "__main__":
